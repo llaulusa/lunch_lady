162e3dcb8a584dbbfa5d71d99ba7276e4e2af9d2
Ladd Laulusa <laddlaulusa@gmail.com>
2015-11-20 14:54:23 -0700

    notes

---
+++ lunch_lady.rb
@@ -0,0 +1,11 @@
+# Lunch Lady Pseudocode:
+# Basic Objectives:
+#   - user chooses from a list of main dishes
+#   - user chooses 2 side dish items
+#   - computer repeats users order
+#   - computer totals lunch items and displays total
+
+# Bonus Objectives:
+#   - user can choose as many "add-on" items as they want before getting total
+#   - user can clear their choices and start over
+#   - user has a wallet total they start with and their choices cannot exceed what they can pay for
