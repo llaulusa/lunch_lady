30270390a024b61b9fd808b1213dfde45240a5b2
Ladd Laulusa <laddlaulusa@gmail.com>
2015-11-23 09:42:58 -0700

    fixing the menu

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -16,28 +16,29 @@
 def order
 
 food={"hotdogs" => 2.50, "hamburgers" => 2.95, "tacos" => 2.99, 
-	"salad" => 1.50, "fruit" => 1.75, "french fries" => 2.25, "coleslaw" => 2.75}
+	"salad" => 1.50, "fruit" => 1.75, "fries" => 2.25, "coleslaw" => 2.75}
 	
 puts "Lunch Lady is here what is your order?"
-puts " Hotdogs"
-puts " Hamburger"
-puts " Tacos"
+puts "Hotdogs"
+puts "Hamburger"
+puts "Tacos"
 
-puts "what is your first selection:"
-main = gets.strip
+puts "What would you like for your main dish?:"
+	main = gets.strip.downcase
 puts "\nHere are the sides:"
-puts " salad"
-puts " fruit"
-puts " french fries"
-puts " coleslaw"
-puts "now pick a side:"
-side_1 = gets.strip
-puts "one more:"
-side_2 = gets.strip
+puts "salad"
+puts "fruit"
+puts "fries"
+puts "coleslaw"
+puts "\nChoose a side:"
+	side_1 = gets.strip.downcase
+puts "One more:"
+	side_2 = gets.strip.downcase
 
- puts "You ordered #{main}, with #{side_1} and #{side_2}"
+ puts "\nYou ordered a #{main}, with #{side_1} and #{side_2}."
 
  # puts "Your total is going to be: $ #{:main + :side_1 + :side_2}"
+puts "Your total is:"
 puts food.fetch(main) + food.fetch(side_1) + food.fetch(side_2)
 
 
